8227f6377491dbefd970868aa1b4eec50b9584af
Katsiaryna Aulaska <klipsakate@gmail.com>
2020-07-31 15:55:24 -0400

    Restructuring.

--- src/index.js
+++ src/index.js
@@ -1,42 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-
-const TodoList = () => {
-
-    const items = ['Learn React', 'Build Awesome App'];
-    return (
-        <ul>
-            <li>{items[0]}</li>
-            <li>{items[1]}</li>
-        </ul>
-    );
-};
-
-const AppHeader = () => {
-    return <h1>My Todo List</h1>;
-};
-
-const SearchPanel = () => {
-    const searchText = 'Type here to search';
-    const searchStyle = {
-        fontSize: '20px'
-    };
-
-    return <input
-        style={searchStyle}
-        placeholder={searchText}
-        disabled //disabled={true}
-    />;
-};
+import AppHeader from "./components/app-header";
+import SearchPanel from "./components/search-panel";
+import TodoList from "./components/todo-list";
 
 const App = () => {
 
-    const value = '<script>alert("")</script>';
-
     return (
         <div>
-            {value}
             <AppHeader/>
             <SearchPanel/>
             <TodoList/>
@@ -45,4 +16,4 @@ const App = () => {
 };
 
 ReactDOM.render(<App/>,
-    document.getElementById('root'));
+    document.getElementById('root')); //куда надо отрендерить элемент
