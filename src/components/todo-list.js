31ddaaeda4b88d3f2bb691780a30a33d02d3a79f
Katsiaryna Aulaska <klipsakate@gmail.com>
2020-07-31 20:39:51 -0400

    Use spread operator for object.

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -4,10 +4,9 @@ import TodoListItem from "./todo-list-item";
 const TodoList = ({todos}) => {
 
         const elements = todos.map((item) => {
-            return (<li>
-                    <TodoListItem
-                        label={item.label}
-                        important={item.important}/>
+            return (
+                <li>
+                    <TodoListItem {...item}/>
                 </li>
             )
         });
