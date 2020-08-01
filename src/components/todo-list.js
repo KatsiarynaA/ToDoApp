7d2011a12dfd354746e0da032644f967d4bbf689
Katsiaryna Aulaska <klipsakate@gmail.com>
2020-07-31 20:31:18 -0400

    Update todo-list.js

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -3,23 +3,35 @@ import TodoListItem from "./todo-list-item";
 
 const TodoList = ({todos}) => {
 
+    const firstEl = (
+        <li>
+            <TodoListItem
+                label={todos[0].label}
+                important={todos[0].important}/>
+        </li>
+    );
+
+    const secondEl = (
+        <li>
+            <TodoListItem
+                label={todos[1].label}
+                important={todos[1].important}/>
+        </li>
+    );
+
+    const thirdEl = (
+        <li>
+            <TodoListItem
+                label={todos[2].label}
+                important={todos[2].important}/>
+        </li>
+    );
+
     return (
         <ul>
-            <li>
-                <TodoListItem
-                    label={todos[0].label}
-                    important={todos[0].important}/>
-            </li>
-            <li>
-                <TodoListItem
-                    label={todos[1].label}
-                    important={todos[1].important}/>
-            </li>
-            <li>
-                <TodoListItem
-                    label={todos[2].label}
-                    important={todos[2].important}/>
-            </li>
+            {firstEl}
+            {secondEl}
+            {thirdEl}
         </ul>
     );
 };
