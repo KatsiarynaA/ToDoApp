0a8d6116769f2d87818a469bf3884a53701489f3
Katsiaryna Aulaska <klipsakate@gmail.com>
2020-07-31 20:36:26 -0400

    Use method map().

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -3,37 +3,21 @@ import TodoListItem from "./todo-list-item";
 
 const TodoList = ({todos}) => {
 
-    const firstEl = (
-        <li>
-            <TodoListItem
-                label={todos[0].label}
-                important={todos[0].important}/>
-        </li>
-    );
+        const elements = todos.map((item) => {
+            return (<li>
+                    <TodoListItem
+                        label={item.label}
+                        important={item.important}/>
+                </li>
+            )
+        });
 
-    const secondEl = (
-        <li>
-            <TodoListItem
-                label={todos[1].label}
-                important={todos[1].important}/>
-        </li>
-    );
-
-    const thirdEl = (
-        <li>
-            <TodoListItem
-                label={todos[2].label}
-                important={todos[2].important}/>
-        </li>
-    );
-
-    return (
-        <ul>
-            {firstEl}
-            {secondEl}
-            {thirdEl}
-        </ul>
-    );
-};
+        return (
+            <ul>
+                {elements}
+            </ul>
+        );
+    }
+;
 
 export default TodoList;
